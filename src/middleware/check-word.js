ae8cf172310e55e9fe0356e747f57b9b16357370
ronbarrantes <ronbarrantes@gmail.com>
2019-04-04 22:45:48 -0700

    check word async

--- src/middleware/check-word.js
+++ src/middleware/check-word.js
@@ -11,30 +11,34 @@ const checkWord = ({ getState, dispatch }) => next => action => {
   const { word, wordList } = getState()
   const { validWords, invalidWords } = wordList
 
+  // TODO: make sure the word is at least 3 letters
+
   if (word.length === 0 || validWords.includes(word) || invalidWords.includes(word)) {
     action.type = NO_WORD_ADDED
     next(action)
     return dispatch(resetWord())
   }
 
-  fetch(apiURL, {
-    method: 'POST',
-    body: JSON.stringify({ word }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then(res => {
-      console.log('FROM CHECKWORD', getState().board)
-      next(action)
-      res.valid ? dispatch(addValidWord(word)):dispatch(addInvalidWord(word))
-
-    })
-    .catch(err=>{
-      console.log('ERROR:', err)
-
-    })
+  apiCall(word, getState, next, action, dispatch)
 }
 
 export default checkWord
+
+async function apiCall(word, getState, next, action, dispatch) {
+  try {
+    const request = await fetch(apiURL, {
+      method: 'POST',
+      body: JSON.stringify({ word }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const result = await request.json()
+    next(action)
+    result.valid ? dispatch(addValidWord(word)) : dispatch(addInvalidWord(word))
+  } catch (err) {
+    console.log('ERROR-->', err)
+  }
+}
+
